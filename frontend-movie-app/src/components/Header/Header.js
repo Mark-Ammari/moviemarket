812d4fad59e86893dc06309713d6a68e6e6bd4a2
Mark Ammari <mark.ammari@gmail.com>
2020-06-03 19:54:43 -0400

    added logout button

--- frontend-movie-app/src/components/Header/Header.js
+++ frontend-movie-app/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classes from './Header.module.css';
 import Logo from '../Logo/Logo';
 import NavButton from '../NavButton/NavButton';
@@ -8,14 +8,23 @@ import { OpenInNewRounded, FavoriteRounded } from '@material-ui/icons';
 import SearchModal from '../SearchModal/SearchModal';
 import { useMediaQuery } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import auth from '../../context/context';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../../store/actions/authUser';
 
 export default function Header() {
     const matches = useMediaQuery('(min-width:767px)');
     const history = useHistory()
+    const dispatch = useDispatch()
+    const authContext = useContext(auth)
     const authenticatePath = () => {
         history.push({
             pathname: "/account/auth"
         })
+    }
+    const logoutUserHandler = () => {
+        dispatch(logoutUser())
+        window.location.reload()
     }
     return (
         <header className={classes.Header}>
@@ -30,9 +39,15 @@ export default function Header() {
                     <NavButton startIcon={<FavoriteRounded />}>
                         Favorites
                     </NavButton>
-                    <NavButton onClick={authenticatePath} startIcon={<OpenInNewRounded />}>
-                        Sign In
-                    </NavButton>
+                    {authContext.isAuth ?
+                        <NavButton onClick={logoutUserHandler} startIcon={<OpenInNewRounded />}>
+                            Sign Out
+                        </NavButton>
+                        :
+                        <NavButton onClick={authenticatePath} startIcon={<OpenInNewRounded />}>
+                            Sign In
+                        </NavButton>
+                    }
                 </nav>
             </div>
         </header>
