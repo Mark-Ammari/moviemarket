521a82500b4476c18693abc2e42ad2156df3c46e
Mark A <mark.ammari@gmail.com>
2022-02-22 10:52:26 -0500

    Updated Footer

--- frontend-movie-app/src/components/Footer/Footer.js
+++ frontend-movie-app/src/components/Footer/Footer.js
@@ -7,7 +7,6 @@ export default function Footer() {
     return (
         <footer
             className={classes.Footer}>
-            <p>Created by Mark Ammari</p>
             <div>
                 <a rel="noopener noreferrer" target="_blank" href="https://github.com/Mark-Ammari"><img src={github} alt="github" /></a>
                 <a rel="noopener noreferrer" target="_blank" href="https://github.com/Mark-Ammari/moviemarket"><img src={sourcecode} alt="source-code" /></a>
